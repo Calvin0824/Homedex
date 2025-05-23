2e0b173a3ad022a6cd4261c82dbb900da06db36e
Calvin Li <calvinli0824@gmail.com>
2025-05-23 14:39:37 -0400

    Initial Commit

---
+++ Homedex/HomedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomedexApp.swift
+//  Homedex
+//
+//  Created by Calvin Li on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct HomedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomedexTests/HomedexTests.swift
@@ -0,0 +1,16 @@
+//
+//  HomedexTests.swift
+//  HomedexTests
+//
+//  Created by Calvin Li on 5/23/25.
+//
+
+import Testing
+
+struct HomedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
